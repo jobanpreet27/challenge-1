71c5356c37c3a6b62311f1b38a1c1b28f18090fb
jobanpreet27 <57243235+jobanpreet27@users.noreply.github.com>
2020-12-30 16:58:24 -0600

    sever connection to index page

--- server/app.js
+++ server/app.js
@@ -1,1 +1,9 @@
 const express = require('express')
+const app = express()
+const path = require('path')
+app.get('',(req, res)=> {
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+})
+app.listen(3000,()=> {
+    console.log('server is up')
+})
