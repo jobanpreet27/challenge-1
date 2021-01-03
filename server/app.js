6357c975f34c845cec4fdecdb464ea5a37460bfd
jobanpreet27 <57243235+jobanpreet27@users.noreply.github.com>
2021-01-02 22:49:19 -0600

    port number fixed

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const port = process.env.PORT || 3000
 app.get('',(req, res)=> {
     res.sendFile(path.join(__dirname,'../public/index.html'))
 })
-app.listen(3000,()=> {
-    console.log('server is up')
+app.listen(port,()=> {
+    console.log('server is up' + port)
 })
